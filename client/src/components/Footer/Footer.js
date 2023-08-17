47f8de4a2ccbc09c9de17b989c891f62d9ff1049
BlueBeam321 <bluebeam321@gmail.com>
2023-08-18 04:40:17 +0900

    update project...

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeartTwoTone } from "@ant-design/icons";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Made with <HeartTwoTone twoToneColor="#eb2f96" /> By Blue
+    </footer>
+  );
+};
+
+export default Footer;
